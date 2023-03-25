bf5b37a6c1a6811ce724c013dbc1525faa3fd1a6
Alexandre <alexandre.souzafilho@hotmail.com>
2023-03-25 18:28:51 -0300

    finished change animation

---
+++ js/login.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const loginContainer = document.getElementById('login-container')
+
+const moveOverlay = () => loginContainer.classList.toggle('move')
+
+// quando ele pefa o elemento 'open-register' e adiciona um evento(execute o evento), evento click. O evento chama a função moveOverlay
+document.getElementById('open-register').addEventListener('click', moveOverlay)
+
+document.getElementById('open-login').addEventListener('click', moveOverlay)
