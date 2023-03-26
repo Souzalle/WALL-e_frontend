0ab916a426c9fd3921eb871a98869f17dd47116d
Alexandre <alexandre.souzafilho@hotmail.com>
2023-03-26 18:44:43 -0300

    add responsiveness

--- js/login.js
+++ js/login.js
@@ -6,5 +6,7 @@ const moveOverlay = () => loginContainer.classList.toggle('move')
 
 // quando ele pefa o elemento 'open-register' e adiciona um evento(execute o evento), evento click. O evento chama a função moveOverlay
 document.getElementById('open-register').addEventListener('click', moveOverlay)
-
 document.getElementById('open-login').addEventListener('click', moveOverlay)
+
+document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
+document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
